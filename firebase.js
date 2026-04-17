6e8ddf72bfe1a9fc194715c93329120ed83e1452
bindhanibikash71-dot <bindhanibikash71@gmail.com>
2026-04-18 05:22:15 +0530

    Update Firebase configuration details
    Updated Firebase configuration with new project details.

--- firebase.js
+++ firebase.js
@@ -1,16 +1,15 @@
-cat << 'EOF' > firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore, collection, addDoc, getDocs, updateDoc, doc, query, where, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// REPLACE WITH YOUR FIREBASE CONFIG
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyCcbXPWvejkZs79ZkHt5m_LHwu78xgtZqI",
+  authDomain: "myaccadamy-b3484.firebaseapp.com",
+  databaseURL: "https://myaccadamy-b3484-default-rtdb.firebaseio.com",
+  projectId: "myaccadamy-b3484",
+  storageBucket: "myaccadamy-b3484.firebasestorage.app",
+  messagingSenderId: "486030293623",
+  appId: "1:486030293623:web:9489839eb1d806b271a472"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -19,4 +18,3 @@ const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
 
 export { auth, db, googleProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged, collection, addDoc, getDocs, updateDoc, doc, query, where, serverTimestamp };
-EOF
